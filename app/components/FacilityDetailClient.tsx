59c7eb171a8b39e10b785fd88ad5883e7854b833
Ammaz Rafi <ammaz@strugbitsglobal.com>
2025-10-09 21:06:16 +0500

    add

--- app/components/FacilityDetailClient.tsx
+++ app/components/FacilityDetailClient.tsx
@@ -358,7 +358,7 @@ export default function FacilityDetailClient({ slug }: FacilityDetailClientProps
         const fetchFacilityDetails = async () => {
             setIsLoading(true);
             const facilityName = slug.replace(/-/g, ' ');
-            const API_URL = process.env.NEXT_PUBLIC_API_URL + '/api/facilities/details';
+            const API_URL = process.env.NEXT_PUBLIC_API_URL + '/facilities/details';
             const params = new URLSearchParams({ name: facilityName }); 
             const url = `${API_URL}?${params.toString()}`;
             
